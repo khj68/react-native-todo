f2a5f788e7d421c07dc51f526038d2f5ad9e26fe
khj68 <maga40@naver.com>
2020-11-20 07:25:11 +0900

    Add navigation

--- todo/index.js
+++ todo/index.js
@@ -2,8 +2,26 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+// import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+// import {name as appName} from './app.json';
+import { Navigation } from 'react-native-navigation';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+Navigation.registerComponent('com.todo.WelcomeScreen', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children: [
+                    {
+                        component: {
+                            name: 'com.todo.WelcomeScreen'
+                        }
+                    }
+                ]
+            }
+        }
+    })
+})
